7f32dd8068443abb78c801c909290126136f0aae
Ofir Popowski <ofirpopowski@hotmail.com>
2020-12-05 19:05:55 +0200

    Improve day 5 solution with binary

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -11,11 +11,15 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut highest_id = 0;
     let mut existing_ids: Vec<i32> = Vec::new();
     for line in content.lines() {
-        let rows = &line[0..7];
-        let columns = &line[7..10];
-        let rows = calculate_step(&rows.to_string(), 6, 127, 0);
-        let columns = calculate_step(&columns.to_string(), 2, 7, 0);
-        let id = rows * 8 + columns;
+        let binary_string: String = line
+            .chars()
+            .map(|c| match c {
+                'F' | 'L' => '0',
+                'B' | 'R' => '1',
+                _ => panic!(),
+            })
+            .collect();
+        let id = i32::from_str_radix(&binary_string, 2).unwrap();
         existing_ids.push(id);
         if id > highest_id {
             highest_id = id;
@@ -27,43 +31,10 @@ fn main() -> Result<(), Box<dyn Error>> {
             let lower = existing_ids[i];
             let higher = existing_ids[i + 1];
             if higher - lower == 2 {
-                println!("SEAT {}", higher - 1);
+                println!("Available seat: {}", higher - 1);
             }
         }
     });
     println!("Highest ID: {}", highest_id);
     Ok(())
-}
-
-fn calculate_step(code: &String, current_power: u32, upper_bound: i32, lower_bound: i32) -> i32 {
-    if !code.is_empty() {
-        let chars: Vec<char> = code.chars().collect();
-        let updated_code = &code[1..code.len()].to_string();
-        let updated_power = if current_power == 0 {
-            0
-        } else {
-            current_power - 1
-        };
-        match chars[0] {
-            'F' | 'L' => calculate_step(
-                updated_code,
-                updated_power,
-                upper_bound - 2_i32.pow(current_power),
-                lower_bound,
-            ),
-            'B' | 'R' => calculate_step(
-                updated_code,
-                updated_power,
-                upper_bound,
-                lower_bound + 2_i32.pow(current_power),
-            ),
-            _ => panic!(),
-        }
-    } else {
-        if upper_bound != lower_bound {
-            panic!("Bounds aren't equal!")
-        } else {
-            upper_bound
-        }
-    }
 }
